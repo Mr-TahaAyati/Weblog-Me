e19fd3bafd2f8ab2cebd68741d7246862571e6dd
TahaAyati <TahaAyati2007@gmail.com>
2025-01-01 20:10:19 +0330

    feat: add Entities classes for DataLayer

---
+++ DataLayer/Entities/Post.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Entities
+{
+  public class Post
+    {
+        [Key]
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Slug { get; set; }
+
+        [ForeignKey("UserId")]
+        public User User { get; set; }
+
+        public ICollection<PostComment> PostComments { get; set; }
+
+    }
+}
---
+++ DataLayer/Entities/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Entities
+{
+  public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string FullName { get; set; }
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public string Password { get; set; }
+        public UserRole Role { get; set; }
+
+
+        public ICollection<Post> Posts { get; set; }
+        public ICollection<PostComment> PostComments { get; set; }
+
+    }
+    public enum UserRole
+    {
+        Admin,
+        User
+    }
+}
