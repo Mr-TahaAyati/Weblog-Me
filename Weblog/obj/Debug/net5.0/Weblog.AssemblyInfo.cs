a0d49c5cdaa3b552df15b2b01fbce94ea234792b
TahaAyati <TahaAyati2007@gmail.com>
2025-01-07 18:01:47 +0330

    feat(styles): apply Shabnam font to error messages

--- Weblog/obj/Debug/net5.0/Weblog.AssemblyInfo.cs
+++ Weblog/obj/Debug/net5.0/Weblog.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Weblog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+850a073c7643dcb4de3044cbefc90c5e32282124")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0640941afa530a35b93c99d5f49169a5def04d37")]
 [assembly: System.Reflection.AssemblyProductAttribute("Weblog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Weblog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
