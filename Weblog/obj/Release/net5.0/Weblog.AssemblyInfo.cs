b1d3a0a0e167ee0f9c2f9f065e580139d96bf890
TahaAyati <TahaAyati2007@gmail.com>
2025-02-14 17:23:03 +0330

    Fixed navbar bug and added Admin button

--- Weblog/obj/Release/net5.0/Weblog.AssemblyInfo.cs
+++ Weblog/obj/Release/net5.0/Weblog.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Weblog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc5ededd13a65709908f1695fe4fbe5063d74349")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b7f1eeb4dd0b7302b26b44897e8737ab7d98ef0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Weblog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Weblog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Weblog/obj/Release/net5.0/Weblog.RazorTargetAssemblyInfo.cs
+++ Weblog/obj/Release/net5.0/Weblog.RazorTargetAssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyProductAttribute("Weblog")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc5ededd13a65709908f1695fe4fbe5063d74349")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b7f1eeb4dd0b7302b26b44897e8737ab7d98ef0")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Weblog.Views")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
